4d39c8d549545f74a9f691163db9a68b7ee6932b
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-07-25 13:34:43 +0530

    Connection Setup b/w backend & MongoDB by Mongoose

--- Day2/server.js
+++ Day2/server.js
@@ -4,14 +4,29 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var mongoose = require('mongoose'); // After referencing mongoose in a variable, we will connect DB and for that we will need the connection string available on cloud.mongodb.com.
+
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// We copied the connection string (URL/URI) of the we want to connect and which is present on cloud.mongodb.com.
+// After pasting the URI, we put in the user credentials by changing the part of the URI string from "<password>" to the password we earlier set for the DB on the cloud (in this case "user").
+// In a production environment , we would want to keep this URL, especially the credentials, hidden in a configuration file that has a safe location.
+// Now we will connect to mongoose as below.
+var dbUrl = 'mongodb+srv://user:user@learning-node.4recu.mongodb.net/?retryWrites=true&w=majority';
 
 var messages = [
   { name: 'Chinmaya', message: 'How are you?' },
   { name: 'Professor X', message: 'I am good, what about you?' },
 ];
+
+// Connecting to mongoDB
+// We supply dbURL(connection string that has auth credentials and reference to DB we created on cloud) as first parameter and callback function as second parameter
+// With mongoose 5 & above we don't need to provide useMongoClient option. Mongoose 5 is using Mongo client by default.
+mongoose.connect(dbUrl, (err) => {
+  console.log('mongoDB Connection Error:', err);
+});
 
 app.get('/messages', (req, res) => {
   res.send(messages);
